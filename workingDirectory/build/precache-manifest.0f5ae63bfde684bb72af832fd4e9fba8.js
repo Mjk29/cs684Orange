1d1b2686c8dbacbf080cc70b29d79e77ddc4bcd0
MK <mjk29@njit.edu>
2019-04-22 04:06:34 -0400

    recent build

---
+++ workingDirectory/build/precache-manifest.0f5ae63bfde684bb72af832fd4e9fba8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac7df46f270b0e2c0f12",
+    "url": "/static/css/main.bf264c22.chunk.css"
+  },
+  {
+    "revision": "ac7df46f270b0e2c0f12",
+    "url": "/static/js/main.2f19ade7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bb92e7f01a5d97d16dda",
+    "url": "/static/css/2.996e685b.chunk.css"
+  },
+  {
+    "revision": "bb92e7f01a5d97d16dda",
+    "url": "/static/js/2.cf319a20.chunk.js"
+  },
+  {
+    "revision": "c1fc3916594e23ee5f868ada2f4cef3b",
+    "url": "/index.html"
+  }
+];
